d18801dcf155afc5309d5ee0bc4cc838b9690427
Jamie Ahmed <jamieahmed273@gmail.com>
2022-09-23 10:48:23 -0400

    add signup & signin controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,57 @@
+import mongoose from 'mongoose'
+import User from '../models/user.js'
+import { theError } from '../middleware/error.js';
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+const SALT_ROUNDS = 10
 
-function signup(req, res) {
-    res.json("you are succuessfully signup")
+
+const signup = async (req, res, next) => {
+    try {
+        const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser = new User({ ...req.body, password: hash });
+        await newUser.save();
+        res.status(200).send("User has been created Successfully!");
+
+    } catch (error) {
+        next(err);
+    }
 }
 
-function signin(req, res) {
+const signin = async (req, res, next) => {
 
-    res.json("sign In successfully")
+    try {
+        const user = await User.findOne({ name: req.body.name });
+        if (!user) return next(theError(404, "User not found!"));
+
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isCorrect) return next(theError(400, "Wrong Credentials!"));
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET);
+        const { password, ...others } = user._doc;
+
+        res
+            .cookie("access_token", token, {
+                httpOnly: true,
+            })
+            .status(200)
+            .json(others);
+
+    } catch (error) {
+        next(err);
+    }
 }
 
-function googleAuth(req, res) {
-    res.json("sign up with google")
+const googleAuth = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
 }
+
 
 
 
