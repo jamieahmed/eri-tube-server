eedb35279748f09ce48d711a732ae23aafd1fb71
Jamie Ahmed <jamieahmed273@gmail.com>
2022-09-25 11:08:36 -0400

    done with ad & get  comment controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,20 +1,43 @@
-const getComments = async (req, res, next) => {
+import { Comment } from "../models/comment.js"
+import { Video } from "../models/video.js";
+import { theError } from "../middleware/error.js"
 
+const getComments = async (req, res, next) => {
+    try {
+        const comments = await Comment.find({ videoId: req.params.videoId });
+        res.status(200).json(comments);
+    } catch (err) {
+        next(err);
+    }
 }
 
 const addComment = async (req, res, next) => {
-
+    const newComment = new Comment({ ...req.body, userId: req.user.id });
+    try {
+        const savedComment = await newComment.save();
+        res.status(200).send(savedComment);
+    } catch (err) {
+        next(err);
+    }
 }
 
 const updateComment = async (req, res, next) => {
-
+    try {
+        res.json('here is updated comment')
+    } catch (error) {
+        next(error)
+    }
 }
 
 const deleteComment = async (req, res, next) => {
-
+    try {
+        res.json("your comment successfully deleted")
+    } catch (err) {
+        next(err);
+    }
 }
 
-export{
+export {
     getComments,
     addComment,
     updateComment,
