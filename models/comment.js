821d74686f831d320f0243037a969af6b6cfe493
Jamie Ahmed <jamieahmed273@gmail.com>
2022-09-25 10:33:07 -0400

    add comment schema

--- models/comment.js
+++ models/comment.js
@@ -2,9 +2,23 @@ import mongoose from "mongoose";
 
 
 const commentSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    videoId: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
 
+},
+    { timestamps: true }
 
-})
+);
 
 const Comment = mongoose.model('Comment', commentSchema)
 
